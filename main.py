f4993f5a73103b96b1e5e0faa6f5d6f86a9b8af3
alon00219 <alon00219@gmail.com>
2026-04-16 00:08:44 +0300

    single post for the browser as well

--- main.py
+++ main.py
@@ -36,6 +36,15 @@ def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts, "title": "Home"},)
 
 
+@app.get("/posts/{post_id}", include_in_schema=False)
+def get_post(request: Request, post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            title = post['title'][:50]
+            return templates.TemplateResponse(request, "post.html", {"post": post, "title": title},)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
+
+
 
 @app.get("/api/posts")
 def get_posts():
